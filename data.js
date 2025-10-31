a1e74e2d4054937c9d9f971361cc5f72c4bce538
brother12334 <ferbyablon@gmail.com>
2025-10-31 15:14:14 -0400

    Add muscle keywords for identification

---
+++ data.js
@@ -0,0 +1,11 @@
+// keyword-based muscle identification
+window.MUSCLE_KEYWORDS = {
+  chest: ["bench", "press", "pushup", "fly"],
+  back: ["row", "pullup", "lat", "deadlift"],
+  legs: ["squat", "lunge", "leg press"],
+  shoulders: ["overhead", "lateral", "arnold", "deltoid"],
+  arms: ["curl", "tricep", "bicep", "dip"],
+  core: ["plank", "crunch", "situp", "ab"],
+  glutes: ["hip thrust", "glute", "bridge"],
+  calves: ["calf"],
+};
